c6518ae0e0392842db6942f122f64df62de6b8f6
Tim Gfrerer <tim.gfrerer@oo43.com>
2011-07-01 00:15:03 +0100

    updated copyright notice to hold our names.
    Signed-off-by: Tim Gfrerer <tim.gfrerer@oo43.com>

--- src/ofxNetworkedParameters.cpp
+++ src/ofxNetworkedParameters.cpp
@@ -1,5 +1,7 @@
 /**
  *  ofxNetworkedParameters
+ *
+ *   Copyright (c) 2011, Tim Gfrerer, James George
  *	
  * Permission is hereby granted, free of charge, to any person
  * obtaining a copy of this software and associated documentation
--- src/ofxNetworkedParameters.h
+++ src/ofxNetworkedParameters.h
@@ -1,6 +1,8 @@
 /**
  *  ofxNetworkedParameters
  *	
+ *   Copyright (c) 2011, Tim Gfrerer, James George
+ * 
  * Permission is hereby granted, free of charge, to any person
  * obtaining a copy of this software and associated documentation
  * files (the "Software"), to deal in the Software without
